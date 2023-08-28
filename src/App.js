7e336553128164e5c8508fc206d0091405a558ae
mayefa10 <mayefa10@yahoo.com>
2023-08-28 14:21:00 -0500

    create project with React js and bootstrap

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">
+          Orion Movies
+        </a>
+      </header>
+      <main>
+        List movies
+      </main>
+    </div>
+  );
+}
+
+export default App;
